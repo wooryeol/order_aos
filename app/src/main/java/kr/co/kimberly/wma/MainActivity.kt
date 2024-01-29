2b954f09f1ba2c5f9bc10c063cb06b8f69bf307f
WOORYEOL <dnfuf0703@gmail.com>
2024-01-29 13:26:32 +0900

    @date : 2024-01-29 @author : 김우렬 @version : 1.0.0:20240126 @comment · 메인 바인딩 추가 · DB 및 DB 모델 추가

--- app/src/main/java/kr/co/kimberly/wma/MainActivity.kt
+++ app/src/main/java/kr/co/kimberly/wma/MainActivity.kt
@@ -1,11 +1,25 @@
 package kr.co.kimberly.wma
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.kimberly.wma.databinding.ActivityMainBinding
+import kr.co.kimberly.wma.db.DBHelper
+import kr.co.kimberly.wma.model.OrderTempList
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mContext: Context
+    private lateinit var mActivity: Activity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mContext = this
+        mActivity = this
     }
 }
